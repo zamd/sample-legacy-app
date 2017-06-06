d9486bb91d0d05a29793d77ec216df8cd151548d
Zulfiqar Ahmed <zamd@Zulfiqars-MacBook-Pro.local>
2017-06-06 14:58:58 +0500

    header display

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const prefix = 'accept';
+  
+  const hrd = Object.keys(req.headers).filter(k=>k.startsWith(prefix)).map(k=>({name:k, value: req.header(k)}));
+
+  res.render('index', { title: 'Legacy App', headers: hrd });
 });
 
 module.exports = router;
